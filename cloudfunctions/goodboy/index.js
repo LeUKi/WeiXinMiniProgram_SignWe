98abf7c2035b8a7e35bd28a112e2e1c5cc1585ff
unknown <unknown@example.com>
2020-10-25 11:29:58 +0800

    细节修改 排名样式逻辑

--- cloudfunctions/goodboy/index.js
+++ cloudfunctions/goodboy/index.js
@@ -9,24 +9,33 @@ const _ = db.command
 // 云函数入口函数
 exports.main = async (event, context) => {
   var newC = []
+  var yesterday = []
   for (let i = 0; i < 20; i++) {
     newC[i] = true
   }
   const a = await db.collection('check').where({
     "daysum": _.neq(0)
-  }).get()
-  const b = await db.collection('chairs').where({ _id: "chairs" }).update({
+  }).orderBy('daysum', 'desc').get()
+  for (let i = 0; i < a.data.length; i++) {
+    yesterday.push({ name: a.data[i].name, daysum: a.data[i].daysum })
+  }
+  await db.collection('chairs').where({ _id: "yesterday" }).update({
+    data: {
+      yesterday: yesterday
+    }
+  })
+ await db.collection('chairs').where({ _id: "chairs" }).update({
     data: {
       chairs: newC
     }
   })
-  const c = await db.collection('check').where({})
+  await db.collection('check').where({})
     .update({
       data: {
         finalCheck: true,
         daysum: 0
       }
     })
-  // )
-  return a
+
+  return "666"
 }
